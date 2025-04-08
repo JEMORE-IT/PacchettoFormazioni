0e1a6c7a22390f40b4a54c124d551160cf99cb8e
AngeLorenzo04 <dicandiangelorenzo@gmail.com>
2025-04-08 10:46:09 +0200

    creato footer

---
+++ app/components/footer.tsx
@@ -0,0 +1,11 @@
+export default function Footer() {
+    return (
+      <footer className="py-6 border-t text-center text-sm text-gray-500">
+        <p>©2025 Blog Minimal — Tutti i diritti riservati.</p>
+        <p className="mt-1">
+          Creato con ❤️ in React e Tailwind
+        </p>
+      </footer>
+    );
+  }
+  
--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,13 @@
+import Footer from "./components/footer";
 import Header from "./components/header";
 import Hero from "./components/hero";
 
 export default function Home() {
   return (
-    <>
-    <Header/>
-    <Hero/>
-    </>
+    <div className="flex flex-col ">
+      <Header/>
+      <Hero/>
+      <Footer/>
+    </div>
   );
 }
