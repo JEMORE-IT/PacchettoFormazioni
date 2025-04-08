8cbe717b5a694ee7de54672effab4a3a4a9e66c0
AngeLorenzo04 <dicandiangelorenzo@gmail.com>
2025-04-08 17:05:00 +0200

    add popup

--- app/components/hero.tsx
+++ app/components/hero.tsx
@@ -1,28 +1,56 @@
+"use client";
+
+import { useState } from "react";
 import posts from "../utils/posts";
+import Popup from "./popup";
+
 
 export default function Hero() {
-    return (
-      <section className="min-h-screen bg-gray-50 py-12 px-4">
-        <div className="max-w-3xl mx-auto">  
-          <div className="space-y-10">
-            {posts.map((post) => (
-              <a
-                key={post.id}
-                className="cursor-pointer"
-                href={`/post/${post.id}`}
-              >
-                <article className="bg-white border border-gray-200 rounded-xl shadow-sm p-6 hover:shadow-md transition mb-5">
-                  <h2 className="text-2xl font-semibold text-blue-500 mb-2">{post.title}</h2>
-                  <p className="text-gray-700 leading-relaxed">
-                    {post.content.slice(0, 69)}
-                    {post.content.length > 70 && " ..."}
-                  </p>
-                </article>
-              </a>
-            ))}
-          </div>
+  const [showPopup, setShowPopup] = useState(false);
+
+  const handleSubmit = (newPost: { title: string; content: string }) => {
+    console.log("Nuovo post:", newPost);
+    // qui puoi gestire l'aggiunta dinamica allo stato se usi uno useState con la lista
+  };
+
+  return (
+    <section className="min-h-screen bg-gray-50 py-12 px-4">
+      <div className="max-w-3xl mx-auto">
+        <div className="mb-8 text-center">
+
+          {/*pulsante per mostrare il popup*/}
+          <button
+            onClick={() => setShowPopup(true)}
+            className="bg-blue-500 hover:bg-blue-600 text-white font-semibold py-2 px-4 rounded-xl shadow"
+          >
+            Add post
+          </button>
         </div>
-      </section>
-    );
-  }
-  
+
+        {/*Se showPopup è true allora mostra il popup*/}
+        {showPopup && (
+          <Popup onClose={() => setShowPopup(false)} onSubmit={handleSubmit} />
+        )}
+
+        {/*lista di articoli*/}
+        <div className="space-y-10">
+        {posts.map((post) => (
+            <a
+              key={post.id}
+              className="cursor-pointer"
+              href={`/post/${post.id}`}
+            >
+              <article className="bg-white border border-gray-200 rounded-xl shadow-sm p-6 hover:shadow-md transition mb-5">
+                <h2 className="text-2xl font-semibold text-blue-500 mb-2">{post.title}</h2>
+                <p className="text-gray-700 leading-relaxed">
+                  {post.content.slice(0, 69)}
+                  {post.content.length > 70 && " ..."}
+                </p>
+              </article>
+            </a>
+          ))}
+        </div>
+      </div>
+    </section>
+  );
+}
